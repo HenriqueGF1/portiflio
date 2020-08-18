877bf8b97c3f528ca0c008b3f5b195aef9933ae9
Henrique <goncalves.f.henrique@gmail.com>
2020-08-18 14:24:38 -0300

    Primeiro Comit

---
+++ precache-manifest.34b71f1d2fa94880ec91fa029e795e9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adcec47729123d39f3013febac567c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90dbdbd2dc750016ee97",
+    "url": "/static/css/main.7397604e.chunk.css"
+  },
+  {
+    "revision": "7469f1560515815f5ae9",
+    "url": "/static/js/2.4374a305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4374a305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dbdbd2dc750016ee97",
+    "url": "/static/js/main.179f6304.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  }
+]);
